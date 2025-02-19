1e917c259c1867967236ff568039c95f27aca79f
Jan Schweizer <janschweizer-pronsfeld@web.de>
2025-02-19 19:24:06 +0100

    Don't tightly pack the bytes of SimpleDescriptor

--- src/apl/descriptors/simple_descriptor.rs
+++ src/apl/descriptors/simple_descriptor.rs
@@ -11,106 +11,15 @@ const SIMPLE_DESCRIPTOR_SIZE: usize = 8 + 2 * MAX_CLUSTER_COUNT; // 1028
 
 pub struct ApplicationClusterList(Vec<u8, MAX_CLUSTER_COUNT>);
 
-pub struct SimpleDescriptor(Vec<u8, SIMPLE_DESCRIPTOR_SIZE>);
-
-impl SimpleDescriptor {
-    fn new(
-        endpoint: u8,
-        application_profile_identifier: u16,
-        application_device_identifier: u16,
-        application_device_version: u8,
-        application_input_cluster_count: u8,
-        application_input_cluster_list: Option<ApplicationClusterList>,
-        application_output_cluster_count: u8,
-        application_output_cluster_list: Option<ApplicationClusterList>,
-    ) -> Self {
-        let mut value: Vec<u8, SIMPLE_DESCRIPTOR_SIZE> = Vec::new();
-        value.resize(SIMPLE_DESCRIPTOR_SIZE, 0).unwrap();
-
-        value[0] = endpoint;
-        value[1] = (application_profile_identifier >> 8) as u8;
-        value[2] = application_profile_identifier as u8;
-        value[3] = (application_device_identifier >> 8) as u8;
-        value[4] = application_device_identifier as u8;
-        value[5] = application_device_version << 4;
-        value[6] = application_input_cluster_count;
-
-        let mut i = 7;
-        if application_input_cluster_count > 0 && application_input_cluster_list.is_some() {
-            value[i..MAX_CLUSTER_COUNT + i]
-                .copy_from_slice(&application_input_cluster_list.unwrap().0);
-            i += MAX_CLUSTER_COUNT;
-        }
-
-        value[i] = application_output_cluster_count;
-        i += 1;
-
-        if application_output_cluster_count > 0 && application_output_cluster_list.is_some() {
-            value[i..MAX_CLUSTER_COUNT + i]
-                .copy_from_slice(&application_output_cluster_list.unwrap().0);
-        }
-
-        SimpleDescriptor(value)
-    }
-
-    pub fn endpoint(&self) -> u8 {
-        self.0[0]
-    }
-
-    pub fn application_profile_identifier(&self) -> u16 {
-        let upper = self.0[1];
-        let lower = self.0[2];
-        ((upper as u16) << 8) | lower as u16
-    }
-
-    pub fn application_device_identifier(&self) -> u16 {
-        let upper = self.0[3];
-        let lower = self.0[4];
-        ((upper as u16) << 8) | lower as u16
-    }
-
-    pub fn application_device_version(&self) -> u8 {
-        self.0[5] >> 4
-    }
-
-    pub fn application_input_cluster_count(&self) -> u8 {
-        self.0[6]
-    }
-
-    pub fn application_input_cluster_list(&self) -> Option<ApplicationClusterList> {
-        if self.application_input_cluster_count() > 0 {
-            let input_cluster_list = &self.0[7..MAX_CLUSTER_COUNT + 7];
-            Some(ApplicationClusterList(
-                Vec::from_slice(input_cluster_list).unwrap(),
-            ))
-        } else {
-            None
-        }
-    }
-
-    pub fn application_output_cluster_count(&self) -> u8 {
-        if self.application_input_cluster_count() > 0 {
-            self.0[7 + MAX_CLUSTER_COUNT]
-        } else {
-            self.0[7]
-        }
-    }
-
-    pub fn application_output_cluster_list(&self) -> Option<ApplicationClusterList> {
-        if self.application_output_cluster_count() > 0 {
-            let i = if self.application_input_cluster_count() > 0 {
-                8 + MAX_CLUSTER_COUNT
-            } else {
-                8
-            };
-            let output_cluster_list = &self.0[i..MAX_CLUSTER_COUNT + i];
-            Some(ApplicationClusterList(
-                Vec::from_slice(output_cluster_list).unwrap(),
-            ))
-        } else {
-            None
-        }
-    }
+pub struct SimpleDescriptor {
+    endpoint: u8,
+    application_profile_identifier: u16,
+    application_device_identifier: u16,
+    application_device_version: u8,
+    application_input_cluster_count: u8,
+    application_input_cluster_list: Option<ApplicationClusterList>,
+    application_output_cluster_count: u8,
+    application_output_cluster_list: Option<ApplicationClusterList>,
 }
 
 #[cfg(test)]
@@ -138,7 +47,7 @@ mod tests {
             Some(ApplicationClusterList(output_values));
 
         // when
-        let simple_descriptor = SimpleDescriptor::new(
+        let simple_descriptor = SimpleDescriptor {
             endpoint,
             application_profile_identifier,
             application_device_identifier,
@@ -147,32 +56,32 @@ mod tests {
             application_input_cluster_list,
             application_output_cluster_count,
             application_output_cluster_list,
-        );
+        };
 
         // then
-        assert_eq!(simple_descriptor.endpoint(), 42);
-        assert_eq!(simple_descriptor.application_profile_identifier(), 123);
-        assert_eq!(simple_descriptor.application_device_identifier(), 456);
-        assert_eq!(simple_descriptor.application_device_version(), 5);
-        assert_eq!(simple_descriptor.application_input_cluster_count(), 2);
-        assert!(simple_descriptor.application_input_cluster_list().is_some());
+        assert_eq!(simple_descriptor.endpoint, 42);
+        assert_eq!(simple_descriptor.application_profile_identifier, 123);
+        assert_eq!(simple_descriptor.application_device_identifier, 456);
+        assert_eq!(simple_descriptor.application_device_version, 5);
+        assert_eq!(simple_descriptor.application_input_cluster_count, 2);
+        assert!(simple_descriptor.application_input_cluster_list.is_some());
         for i in 0..MAX_CLUSTER_COUNT {
             assert_eq!(
                 simple_descriptor
-                    .application_input_cluster_list()
+                    .application_input_cluster_list
+                    .as_ref()
                     .unwrap()
                     .0[i],
                 (i + 1) as u8
             );
         }
-        assert_eq!(simple_descriptor.application_output_cluster_count(), 1);
-        assert!(simple_descriptor
-            .application_output_cluster_list()
-            .is_some());
+        assert_eq!(simple_descriptor.application_output_cluster_count, 1);
+        assert!(simple_descriptor.application_output_cluster_list.is_some());
         for i in 0..MAX_CLUSTER_COUNT {
             assert_eq!(
                 simple_descriptor
-                    .application_output_cluster_list()
+                    .application_output_cluster_list
+                    .as_ref()
                     .unwrap()
                     .0[i],
                 (i + 2) as u8
@@ -198,7 +107,7 @@ mod tests {
         let application_output_cluster_list: Option<ApplicationClusterList> = None;
 
         // when
-        let simple_descriptor = SimpleDescriptor::new(
+        let simple_descriptor = SimpleDescriptor {
             endpoint,
             application_profile_identifier,
             application_device_identifier,
@@ -207,27 +116,32 @@ mod tests {
             application_input_cluster_list,
             application_output_cluster_count,
             application_output_cluster_list,
-        );
+        };
 
         // then
-        assert_eq!(simple_descriptor.endpoint(), 42);
-        assert_eq!(simple_descriptor.application_profile_identifier(), 123);
-        assert_eq!(simple_descriptor.application_device_identifier(), 456);
-        assert_eq!(simple_descriptor.application_device_version(), 5);
-        assert_eq!(simple_descriptor.application_input_cluster_count(), 2);
-        assert!(simple_descriptor.application_input_cluster_list().is_some());
+        assert_eq!(simple_descriptor.endpoint, 42);
+        assert_eq!(simple_descriptor.application_profile_identifier, 123);
+        assert_eq!(simple_descriptor.application_device_identifier, 456);
+        assert_eq!(simple_descriptor.application_device_version, 5);
+        assert_eq!(simple_descriptor.application_input_cluster_count, 2);
+        assert!(simple_descriptor
+            .application_input_cluster_list
+            .as_ref()
+            .is_some());
         for i in 0..MAX_CLUSTER_COUNT {
             assert_eq!(
                 simple_descriptor
-                    .application_input_cluster_list()
+                    .application_input_cluster_list
+                    .as_ref()
                     .unwrap()
                     .0[i],
                 (i + 1) as u8
             );
         }
-        assert_eq!(simple_descriptor.application_output_cluster_count(), 0);
+        assert_eq!(simple_descriptor.application_output_cluster_count, 0);
         assert!(simple_descriptor
-            .application_output_cluster_list()
+            .application_output_cluster_list
+            .as_ref()
             .is_none());
     }
 
@@ -247,7 +161,7 @@ mod tests {
             Some(ApplicationClusterList(output_values));
 
         // when
-        let simple_descriptor = SimpleDescriptor::new(
+        let simple_descriptor = SimpleDescriptor {
             endpoint,
             application_profile_identifier,
             application_device_identifier,
@@ -256,23 +170,28 @@ mod tests {
             application_input_cluster_list,
             application_output_cluster_count,
             application_output_cluster_list,
-        );
+        };
 
         // then
-        assert_eq!(simple_descriptor.endpoint(), 42);
-        assert_eq!(simple_descriptor.application_profile_identifier(), 123);
-        assert_eq!(simple_descriptor.application_device_identifier(), 456);
-        assert_eq!(simple_descriptor.application_device_version(), 5);
-        assert_eq!(simple_descriptor.application_input_cluster_count(), 0);
-        assert!(simple_descriptor.application_input_cluster_list().is_none());
-        assert_eq!(simple_descriptor.application_output_cluster_count(), 1);
+        assert_eq!(simple_descriptor.endpoint, 42);
+        assert_eq!(simple_descriptor.application_profile_identifier, 123);
+        assert_eq!(simple_descriptor.application_device_identifier, 456);
+        assert_eq!(simple_descriptor.application_device_version, 5);
+        assert_eq!(simple_descriptor.application_input_cluster_count, 0);
         assert!(simple_descriptor
-            .application_output_cluster_list()
+            .application_input_cluster_list
+            .as_ref()
+            .is_none());
+        assert_eq!(simple_descriptor.application_output_cluster_count, 1);
+        assert!(simple_descriptor
+            .application_output_cluster_list
+            .as_ref()
             .is_some());
         for i in 0..MAX_CLUSTER_COUNT {
             assert_eq!(
                 simple_descriptor
-                    .application_output_cluster_list()
+                    .application_output_cluster_list
+                    .as_ref()
                     .unwrap()
                     .0[i],
                 (i + 2) as u8
