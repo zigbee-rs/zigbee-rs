dce4eba96c9b55377926b4bae086702dd83467d6
Patrick Willner <dev@willner.online>
2025-05-08 22:25:37 +0200

    add parse macro

--- src/common/parse.rs
+++ src/common/parse.rs
@@ -130,11 +130,122 @@ macro_rules! impl_byte {
         $(#[$m])*
         $v struct $name($vt $ty);
 
-        impl ::byte::TryRead<'_, ::byte::ctx::Endian> for $name {
-            fn try_read(bytes: &'_ [u8], ctx: ::byte::ctx::Endian) -> byte::Result<(Self, usize)> {
-                let (v, sz) = <$ty>::try_read(bytes, ctx)?;
+        impl<C: ::core::default::Default> ::byte::TryRead<'_, C> for $name {
+            fn try_read(bytes: &'_ [u8], _: C) -> ::byte::Result<(Self, usize)> {
+                let (v, sz) = <$ty>::try_read(bytes, ::byte::BE)?;
                 Ok((Self(v), sz))
             }
         }
+
+        impl<C: ::core::default::Default> ::byte::TryWrite<C> for $name {
+            fn try_write(self, bytes: &mut [u8], _: C) -> ::byte::Result<usize> {
+                self.0.try_write(bytes, ::byte::BE)
+            }
+        }
     };
+    (
+        $(#[$m:meta])*
+        $v:vis struct $name:ident $(<$lifetime:lifetime>)? {
+            $(
+                $(#[doc = $doc:literal])*
+                $(#[ctx = $ctx_hdr:expr])?
+                $(#[parse_if = $parse_if_hdr:expr])?
+                $vf:vis $field_name:ident: $field_ty:ty
+            ),+
+            $(,)?
+        }
+    ) => {
+        $(#[$m])*
+        $v struct $name $(<$lifetime>)? {
+            $(
+                $(#[doc = $doc])*
+                $vf $field_name: $field_ty
+            ),+
+        }
+
+        #[allow(single_use_lifetimes, unreachable_code, unused_variables)]
+        impl<'a, C: ::core::default::Default> ::byte::TryRead<'a, C> for $name $(<$lifetime>)? {
+            fn try_read(bytes: &'a [u8], _: C) -> ::byte::Result<(Self, usize)> {
+                use ::byte::BytesExt;
+                let offset = &mut 0;
+                $(
+                    let ctx = ::byte::BE;
+                    $(
+                        let ctx = $ctx_hdr;
+                    )?
+
+                    let should_read = true;
+                    $(let should_read = $parse_if_hdr;)?
+
+                    let $field_name: $field_ty = if should_read {
+                        let v = bytes.read_with(offset, ctx)?;
+                        $(
+                            let _ = $parse_if_hdr;
+                            let v = Some(v);
+                        )?
+                        v
+                    } else {
+                        (|| {
+                            $(
+                                let _ = $parse_if_hdr;
+                                return None;
+                            )?
+                            unreachable!()
+                        })()
+                    };
+                )+
+                let s = Self {
+                    $($field_name,)+
+                };
+                Ok((s, *offset))
+            }
+        }
+
+        #[allow(single_use_lifetimes, unused_variables)]
+        impl<'a, C: ::core::default::Default> ::byte::TryWrite<C> for $name $(<$lifetime>)? {
+            fn try_write(self, bytes: &mut [u8], _: C) -> ::byte::Result<usize> {
+                use ::byte::BytesExt;
+                let offset = &mut 0;
+
+                let Self {
+                    $($field_name,)+
+                } = self;
+
+                $(
+                    let ctx = ::byte::BE;
+                    $(
+                        let _ = $ctx_hdr;
+                        let ctx = ();
+                    )?
+
+                    let should_write = true;
+                    $(let should_write = $parse_if_hdr;)?
+                    if should_write {
+                        let v = $field_name;
+                        $(
+                            let _ = $parse_if_hdr;
+                            let v = v.unwrap();
+                        )?
+                        bytes.write_with(offset, v, ctx)?;
+                    }
+                )+
+                Ok(*offset)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    impl_byte! {
+        pub struct DataFrame<'a> {
+            #[ctx = ()]
+            pub flag: bool,
+            pub address: crate::common::types::ShortAddress,
+            #[parse_if = flag]
+            pub conditional: Option<u8>,
+            #[ctx = byte::ctx::Bytes::Len(flag as usize)]
+            pub data: &'a [u8],
+        }
+    }
 }
--- src/nwk/frame/mod.rs
+++ src/nwk/frame/mod.rs
@@ -1,2 +1,114 @@
+pub mod command;
 pub mod frame_control;
 pub mod header;
+
+use byte::ctx;
+use byte::BytesExt;
+use byte::TryRead;
+use byte::TryWrite;
+use frame_control::FrameType;
+use header::Header;
+
+/// NWK Frame
+pub enum Frame<'a> {
+    Data(DataFrame<'a>),
+    NwkCommand(CommandFrame<'a>),
+    Reserved(Header<'a>),
+    InterPan(Header<'a>),
+}
+
+impl<'a> TryRead<'a> for Frame<'a> {
+    fn try_read(bytes: &'a [u8], _: ()) -> byte::Result<(Self, usize)> {
+        let offset = &mut 0;
+
+        let header: Header<'a> = bytes.read_with(offset, ())?;
+
+        let frame = match header.frame_control.frame_type() {
+            FrameType::Data => {
+                let data_frame = DataFrame {
+                    header,
+                    payload: bytes.read_with(offset, ctx::Bytes::Len(bytes.len() - *offset))?,
+                };
+                Self::Data(data_frame)
+            }
+            FrameType::NwkCommand => {
+                let cmd_frame = CommandFrame {
+                    header,
+                    command: bytes.read(offset)?,
+                };
+                Self::NwkCommand(cmd_frame)
+            }
+            FrameType::Reserved => Self::Reserved(header),
+            FrameType::InterPan => Self::InterPan(header),
+        };
+
+        Ok((frame, *offset))
+    }
+}
+
+/// NWK Data Frame
+pub struct DataFrame<'a> {
+    pub header: Header<'a>,
+    pub payload: &'a [u8],
+}
+
+/// NWK Command Frame
+pub struct CommandFrame<'a> {
+    pub header: Header<'a>,
+    pub command: Command,
+}
+
+/// Comand Frame Identifiers.
+///
+/// See Section 3.4.
+#[derive(Debug)]
+#[repr(u8)]
+pub enum Command {
+    RouteRequest = 0x01,
+    RouteReply = 0x02,
+    NetworkStatus = 0x03,
+    Leave = 0x04,
+    RouteRecord = 0x05,
+    RejoinRequest = 0x06,
+    RejoinResponse = 0x07,
+    LinkStatus = 0x08,
+    NetworkReport = 0x09,
+    NetworkUpdate = 0x0a,
+    EndDeviceTimeoutRequest = 0x0b,
+    EndDeviceTimeoutResponse = 0x0c,
+    LinkPowerDelta = 0x0d,
+    Reserved,
+}
+
+impl TryRead<'_, ()> for Command {
+    fn try_read(bytes: &'_ [u8], _: ()) -> byte::Result<(Self, usize)> {
+        let offset = &mut 0;
+        let id: u8 = bytes.read(offset)?;
+        let command = match id {
+            0x01 => Self::RouteRequest,
+            0x02 => Self::RouteReply,
+            0x03 => Self::NetworkStatus,
+            0x04 => Self::Leave,
+            0x05 => Self::RouteRecord,
+            0x06 => Self::RejoinRequest,
+            0x07 => Self::RejoinResponse,
+            0x08 => Self::LinkStatus,
+            0x09 => Self::NetworkReport,
+            0x0a => Self::NetworkUpdate,
+            0x0b => Self::EndDeviceTimeoutRequest,
+            0x0c => Self::EndDeviceTimeoutResponse,
+            0x0d => Self::LinkPowerDelta,
+            _ => Self::Reserved,
+        };
+
+        Ok((command, *offset))
+    }
+}
+
+impl TryWrite for Command {
+    fn try_write(self, bytes: &mut [u8], _: ()) -> byte::Result<usize> {
+        let offset = &mut 0;
+        bytes.write(offset, self as u8)?;
+        Ok(*offset)
+    }
+}
