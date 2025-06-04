fee1f11bf1959b73b762912826973c24ccf0618c
Patrick Willner <dev@willner.online>
2025-06-04 14:07:45 +0200

    fix aps frame

--- src/aps/apdu/extended_frame_control.rs
+++ src/aps/apdu/extended_frame_control.rs
@@ -9,8 +9,10 @@ impl_byte! {
     #[derive(Clone, Debug, Eq, PartialEq)]
     pub struct ExtendedFrameControlField {
         pub extended_frame_control: ExtendedFrameControl,
-        pub block_number: u8,
-        pub ack_bitfield: u8,
+        #[parse_if = extended_frame_control.is_fragmented()]
+        pub block_number: Option<u8>,
+        #[parse_if = extended_frame_control.is_fragmented()]
+        pub ack_bitfield: Option<u8>,
     }
 }
 
@@ -21,37 +23,26 @@ impl_byte! {
     }
 }
 
-/// Extended Header Sub-Frame
-///
-/// See Section 2.2.5.1.8
-#[repr(u8)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Fragmentation {
-    NoFragmentation = 0b00,
-    Fragmentation = 0b01,
-    PartOfFragmentedTransmission = 0b10,
-    Reserved = 0b11,
-}
-
-impl TryRead<'_, byte::ctx::Endian> for Fragmentation {
-    fn try_read(bytes: &'_ [u8], _: byte::ctx::Endian) -> byte::Result<(Self, usize)> {
-        let offset = &mut 0;
-        let id: u8 = bytes.read_with(offset, LE)?;
-        let command = match id {
-            0b00 => Self::NoFragmentation,
-            0b01 => Self::Fragmentation,
-            0b10 => Self::PartOfFragmentedTransmission,
-            _ => Self::Reserved,
-        };
-
-        Ok((command, *offset))
+impl ExtendedFrameControl {
+    pub fn is_fragmented(&self) -> bool {
+        matches!(
+            self.fragmentation,
+            Fragmentation::Fragmentation | Fragmentation::PartOfFragmentedTransmission
+        )
     }
 }
 
-impl TryWrite<byte::ctx::Endian> for Fragmentation {
-    fn try_write(self, bytes: &mut [u8], _: byte::ctx::Endian) -> byte::Result<usize> {
-        let offset = &mut 0;
-        bytes.write(offset, self as u8)?;
-        Ok(*offset)
+impl_byte! {
+    /// Extended Header Sub-Frame
+    ///
+    /// See Section 2.2.5.1.8
+    #[repr(u8)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub enum Fragmentation {
+        NoFragmentation = 0b00,
+        Fragmentation = 0b01,
+        PartOfFragmentedTransmission = 0b10,
+        #[fallback = true]
+        Reserved,
     }
 }
