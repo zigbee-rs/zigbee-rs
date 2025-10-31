722e7fe3ee8695e93cd39631f07424327b8c7eb4
Patrick Willner <dev@willner.online>
2025-10-31 13:48:32 +0100

    implement mlme-scan

--- zigbee-mac/src/lib.rs
+++ zigbee-mac/src/lib.rs
@@ -3,10 +3,16 @@
 #[cfg(feature = "esp32c6")]
 pub mod esp;
 
-const MAX_IEEE802154_CANNELS: usize = 27;
+const MAX_IEEE802154_CHANNELS: u8 = 27;
+
+const A_BASE_SLOT_DURATION: u32 = 60;
+const A_NUM_SUPER_FRAME_SLOTS: u32 = 16;
+const A_BASE_SUPER_FRAME_DURATION: u32 = A_BASE_SLOT_DURATION * A_NUM_SUPER_FRAME_SLOTS;
+
+pub enum MacError {}
 
 pub trait Mlme {
-    fn scan_network(&self, ty: ScanType, channels: u32, duration: u8);
+    fn scan_network(&mut self, ty: ScanType, channels: u32, duration: u8) -> Result<(), MacError>;
 }
 
 #[repr(u8)]
@@ -16,4 +22,10 @@ pub enum ScanType {
     Active,
     Passive,
     Orphan,
+}
+
+#[derive(Debug)]
+pub struct ScanResult {
+    pub channel: u8,
+    // ..
 }
